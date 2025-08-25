4966101951afe6d2ce20048e6c6e17308f376447
leeje0506 <ju3391@naver.com>
2025-08-25 17:41:36 +0900

    select_fruit.sh 수행하는 dag 업로드

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -14,7 +14,7 @@ with DAG(
 ) as dag:
     
     t1_orange = BashOperator(
-        task_id = "ti_orange",
+        task_id = "t1_orange",
         bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE",
     )
 
