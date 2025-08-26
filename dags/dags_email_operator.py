20a601a57c25df4a39ac441814b42cf9d111ac63
leeje0506 <ju3391@naver.com>
2025-08-26 09:55:34 +0900

    email_operator 업로드

---
+++ dags/dags_email_operator.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.operators.email import EmailOperator
+
+with DAG(
+    dag_id="dags_email_operator", # airflow에 뜨는 이름, 이거랑 파일이름 맞추기 권장
+    schedule="0 8 1 * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"), # UTC는 글로벌, Asia/seoul
+    catchup=False, # True : 현재 일자랑 start_date 사이의 모든 날짜를 실행해버림
+) as dag:
+    send_email_task = EmailOperator(
+        task_id= "send_email_task",
+        to="ju3391@naver.com",
+        subject="Airflow 성공메일",
+        html_content="Airflow 작업이 완료되었습니다."
+    )
