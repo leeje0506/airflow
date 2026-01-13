c8ed30de44585126a586de9b25f01e3dcf78330c
leeje0506 <ju3391@naver.com>
2026-01-13 11:17:42 +0900

    fix: Asia/Seoul 변경

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -8,7 +8,7 @@ from airflow.operators.email import EmailOperator
 with DAG(
     dag_id="dags_email_operator", # airflow에 뜨는 이름, 이거랑 파일이름 맞추기 권장
     schedule="0 8 1 * *",
-    start_date=pendulum.datetime(2025, 12, 31, tz="Asia/seoul"), # UTC는 글로벌, Asia/seoul
+    start_date=pendulum.datetime(2025, 12, 31, tz="Asia/Seoul"), # UTC는 글로벌, Asia/Seoul
     catchup=False, # True : 현재 일자랑 start_date 사이의 모든 날짜를 실행해버림
 ) as dag:
     send_email_task = EmailOperator(
