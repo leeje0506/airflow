6f55a876606337a2816dbfb919b4292126639e07
leeje0506 <ju3391@naver.com>
2026-01-13 11:10:25 +0900

    fix: "" 변경

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -13,7 +13,7 @@ with DAG(
 ) as dag:
     send_email_task = EmailOperator(
         task_id= "send_email_task",
-        conn_id='conn_smtp_gmail',
+        conn_id="conn_smtp_gmail",
         to="jieun0320@gmail.com",
         subject="Airflow 성공메일",
         html_content="Airflow 작업이 완료되었습니다."
