cba8baef69c045ce87817b2bf5705573054992f7
leeje0506 <ju3391@naver.com>
2026-01-13 14:23:07 +0900

    op_args 테스트용 업로드

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,20 @@
+from airflow.sdk import DAG, task
+import pendulum
+
+from airflow.operators.python import PythonOperator
+from common.common.common_func import regist
+
+with DAG(
+    dag_id="dags_python_with_op_args", # airflow에 뜨는 이름, 이거랑 파일이름 맞추기 권장
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2025, 12, 31, tz="Asia/Seoul"), # UTC는 글로벌, Asia/Seoul
+    catchup=False, # True : 현재 일자랑 start_date 사이의 모든 날짜를 실행해버림
+) as dag:
+
+    regist_t1 = PythonOperator(
+        task_id = 'regist_t1',
+        python_callable = regist,
+        op_args = ['aaa', 'man', 'kr', 'seoul']
+    )
+
+    regist_t1
