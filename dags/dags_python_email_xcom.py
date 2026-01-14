c07201110c24d2b8401e06d6c3ec7ff3406caa74
leeje0506 <ju3391@naver.com>
2026-01-14 14:19:23 +0900

    xcom 테스트용 업로드

--- dags/dags_python_email_xcom.py
+++ dags/dags_python_email_xcom.py
@@ -24,6 +24,7 @@ with DAG(
 
     send_email = EmailOperator(
         task_id='send_email',
+        conn_id='conn_smtp_gmail',
         to='ju3391@naver.com',
         subject='{{ data_interval_end.in_timezone("Asia/Seoul") | ds }} some_logic 처리결과',
         html_content='{{ data_interval_end.in_timezone("Asia/Seoul") | ds }} 처리 결과는 <br> \
